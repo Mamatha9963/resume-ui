442ab35c43c6707566989a61021360562d31ebaf
Balaji Nallam <balajinallam@Balajis-MacBook-Air.local>
2025-07-25 10:39:08 +0530

    reverted changes1

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FormPage from './FormPage'; // Import FormPage component
 import  ResultPage  from './ResultPage'; // Import ResultPage component
 
+console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+console.log("All import.meta.env:", import.meta.env);
+
 const App= () => {
   return (
     <>
